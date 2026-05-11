79bffe9c45984d5d4ff9ee7b3fb0b7433e361dce
Shang Kheat <skheat3007@gmail.com>
2026-05-11 20:19:13 +0800

    Added Chart 3 to html page structure

--- js/chart_embed.js
+++ js/chart_embed.js
@@ -1,4 +1,9 @@
-var spec = "js/chart1.vg.json";
+var chart1 = "js/chart1.vg.json";
+
+var chart3 = "js/chart3.vg.json";
  
-vegaEmbed('#chart1', spec).then(function(result) {
+vegaEmbed('#chart1', chart1).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart3', chart3).then(function(result) {
 }).catch(console.error);
