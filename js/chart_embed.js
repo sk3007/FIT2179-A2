70addd944db402823b69417e43fb1f0f43b15b5e
Shang Kheat <skheat3007@gmail.com>
2026-05-13 00:11:24 +0800

    Added Chat 10 to html page structure

--- js/chart_embed.js
+++ js/chart_embed.js
@@ -5,6 +5,10 @@ var chart4 = "charts/chart4.vg.json";
 var chart5 = "charts/chart5.vg.json";
 var chart6 = "charts/chart6.vg.json";
 
+
+
+var chart10 = "charts/chart10.vg.json";
+
 vegaEmbed('#chart1', chart1).then(function(result) {
 }).catch(console.error);
 
@@ -18,4 +22,7 @@ vegaEmbed('#chart5', chart5).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#chart6', chart6).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart10', chart10).then(function(result) {
 }).catch(console.error);
