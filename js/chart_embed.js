9c2ef9b1e0d2cfe6aaea4439c9d3b5c15863e904
Shang Kheat <skheat3007@gmail.com>
2026-05-13 23:10:56 +0800

    Added Chart 7 to html page structure

--- js/chart_embed.js
+++ js/chart_embed.js
@@ -4,7 +4,7 @@ var chart3 = "charts/chart3.vg.json";
 var chart4 = "charts/chart4.vg.json";
 var chart5 = "charts/chart5.vg.json";
 var chart6 = "charts/chart6.vg.json";
-
+var chart7 = "charts/chart7.vg.json";
 
 var chart9 = "charts/chart9.vg.json";
 var chart10 = "charts/chart10.vg.json";
@@ -19,8 +19,8 @@ vegaEmbed('#chart1', chart1).then(function(result) {
 vegaEmbed('#chart2', chart2).then(function(result) {
 }).catch(console.error);
 
-vegaEmbed('#chart3', chart3).then(function(result) {
-}).catch(console.error);
+// vegaEmbed('#chart3', chart3).then(function(result) {
+// }).catch(console.error);
 
 vegaEmbed('#chart4', chart4).then(function(result) {
 }).catch(console.error);
@@ -29,6 +29,9 @@ vegaEmbed('#chart5', chart5).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#chart6', chart6).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart7', chart7).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed('#chart9', chart9).then(function(result) {
