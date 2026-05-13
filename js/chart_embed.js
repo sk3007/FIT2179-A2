656c09910e305b73be81aff8d5835dda55ec6693
Shang Kheat <skheat3007@gmail.com>
2026-05-13 10:20:25 +0800

    Added Chart 13 & 14 to html page structure

--- js/chart_embed.js
+++ js/chart_embed.js
@@ -6,8 +6,12 @@ var chart5 = "charts/chart5.vg.json";
 var chart6 = "charts/chart6.vg.json";
 
 
-
+var chart9 = "charts/chart9.vg.json";
 var chart10 = "charts/chart10.vg.json";
+
+
+var chart13 = "charts/chart13.vg.json";
+var chart14 = "charts/chart14.vg.json";
 
 vegaEmbed('#chart1', chart1).then(function(result) {
 }).catch(console.error);
@@ -24,5 +28,14 @@ vegaEmbed('#chart5', chart5).then(function(result) {
 vegaEmbed('#chart6', chart6).then(function(result) {
 }).catch(console.error);
 
+vegaEmbed('#chart9', chart9).then(function(result) {
+}).catch(console.error);
+
 vegaEmbed('#chart10', chart10).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart13', chart13).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed('#chart14', chart14).then(function(result) {
 }).catch(console.error);
